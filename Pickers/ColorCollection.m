0f3bccb376c66c5e3d58547c31f989e5792d9ed5
Development <support@chronicstimulation.com>
2017-05-21 11:25:06 -0400

    formatting correction per compiler warning.

--- Pickers/ColorCollection.m
+++ Pickers/ColorCollection.m
@@ -215,7 +215,7 @@
         }   break;
         case CSM_Random:  {
             sortedKeyArray = [NSMutableArray arrayWithArray:self.unsortedKeys];
-            srandom((uInt)time(NULL));
+            srandom((uint)time(NULL));
             for (NSUInteger i = 0; i < objectCount; ++i) {
                 // Select a random element between i and end of array to swap with.
                 NSInteger nElements = objectCount - i;
